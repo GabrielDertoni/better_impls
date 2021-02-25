21ff981af4ac2160d2d20224935bd545332638e8
GabrielDertoni <gab.dertoni@usp.br>
2021-02-25 02:20:01 -0300

    Included documentation and more modular code

--- src/lib.rs
+++ src/lib.rs
@@ -8,95 +8,83 @@ use proc_macro::TokenStream;
 use syn::{ parse_macro_input, parse_quote, ItemFn };
 use quote::{ quote, format_ident };
 
+///
+/// The `impl_iterator` attribute is used for when a function will return an
+/// `impl Iterator<Item = type>` (where `type` is some concrete type) but does
+/// not require every place the function returns to use the same concrete type
+/// that implements `Iterator`. This is an alternative to returning a
+/// `Box<dyn Iterator<Item = type>>` that does not require dynamic dispatch
+/// because the size of the return type is known at compile time.
+/// 
+/// # Exaples
+///
+/// ```
+/// use std::iter;
+///
+/// #[impl_iterator]
+/// fn generate_numbers(n: usize) -> impl Iterator<Item = usize> {
+///     if n == 0 {
+///         // Here the concrete type would be iter::Empty
+///         iter::empty::<usize>()
+///     } else if n == 1 {
+///         // Concrete type: iter::Once
+///         iter::once(1)
+///     } else {
+///         let count = 0;
+///         // Concrete type: iter::FromFn
+///         iter::from_fn(move || {
+///             count += 1;
+///             
+///             if count <= n {
+///                 Some(count)
+///             } else {
+///                 None
+///             }
+///         })
+///     }
+/// }
+/// ```
+///
+/// # Things you are still **not** able to do
+///
+/// ```
+/// use std::iter;
+///
+/// #[impl_iterator]
+/// fn generate_numbers(n: usize) -> impl Iterator<Item = usize> {
+///     if n == 0 {
+///         iter::empty::<usize>()
+///     } else if n == 1 {
+///         iter::once(1)
+///     } else {
+///         // This will cause a compiler error because now the compiler has to
+///         // figure out a concrete type for the `chain` expression, but that
+///         // would mean first figuring out the type for `generate_numbers`
+///         // which is what we were trying to figure out in the first place.
+///         // Because of this, here a boxed dyn is needed.
+///         generate_numbers(n - 1).chain(iter::once(n))
+///     }
+/// }
+/// ```
+///
 
 #[proc_macro_attribute]
 pub fn impl_iterator(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut ast = parse_macro_input!(item as ItemFn);
 
-    use syn::{ ReturnType
-             , Type
-             , TypeImplTrait
-             , TypeParamBound
-             , TraitBound
-             , Path
-             , PathSegment
-             , PathArguments
-             , AngleBracketedGenericArguments
-             , GenericArgument
-             , Binding
-             };
-
-    let item_type = match ast.sig.output {
-        ReturnType::Type(_, box Type::ImplTrait(TypeImplTrait { ref mut bounds, .. })) => {
-            if bounds.len() != 1 {
-                panic!("Expected an impl Iterator as return type");
-            }
-
-            // Assert the function has `impl Iterator` as return type.
-            if let TypeParamBound::Trait(
-                TraitBound {
-                    path: Path {
-                        segments,
-                        ..
-                    },
-                    ..
-                }
-            ) = bounds.first().unwrap() {
-                let PathSegment { ident, .. } = segments.last().unwrap();
-                if ident != "Iterator" {
-                    panic!("Expected an impl Iterator as return type.");
-                }
-
-                if let PathSegment {
-                    arguments: PathArguments::AngleBracketed(
-                        AngleBracketedGenericArguments {
-                            args,
-                            ..
-                        }
-                    ),
-                    ..
-                } = segments.last().unwrap() {
-                    if let GenericArgument::Binding(Binding { ty, .. }) = args.first().unwrap() {
-                        ty.clone()
-                    } else {
-                        panic!("Expected binding in Iterator Item type")
-                    }
-                } else {
-                    panic!("Expected angle bracketed generic arguments")
-                }
-            } else {
-                unreachable!();
-            }
-        },
-        _ => panic!("Expected an impl Iterator as return type."),
-    };
+    let item_type = get_item_type(&ast.sig.output);
 
     let type_name = format_ident!("BetterImpl{}", ast.sig.ident.to_string());
     let returns_count = overwrite_all_return_exprs(&mut ast, type_name.clone());
 
-    let mut i = 0;
-    let enum_variants: Vec<_> = std::iter::from_fn(move || -> Option<syn::Ident> {
-        i += 1;
-        if i <= returns_count {
-            Some(format_ident!("Return{}", i))
-        } else {
-            None
-        }
-    }).collect();
-
-    let mut i = 0;
-    let enum_generics: Vec<_> = std::iter::from_fn(move || -> Option<syn::GenericParam> {
-        i += 1;
-        if i <= returns_count {
-            let generic = format_ident!("T{}", i);
-            Some(parse_quote!{ #generic })
-        } else {
-            None
-        }
-    }).collect();
+    let enum_variants = generate_enum_variants(returns_count);
+    let enum_generics = generate_generics(returns_count);
 
     let stream = quote! {
-        enum #type_name<#(#enum_generics),*> {
+        enum #type_name<#(#enum_generics),*>
+        where
+            #(#enum_generics: Iterator<Item = #item_type>),*
+        {
             #(#enum_variants(#enum_generics)),*
         }
 
@@ -116,6 +104,9 @@ pub fn impl_iterator(_attr: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(stream)
 }
 
+// Overwrite all expressions where the function returns by a special generated
+// enum variant. In this way, every different enum variant of the same enum,
+// which makes them all the same type.
 fn overwrite_all_return_exprs<'a>(item: &'a mut syn::ItemFn, type_name: syn::Ident) -> usize {
     let mut count = 0;
     overwrite_all_return_exprs_in_block(item.block.as_mut(), &mut count, false, type_name);
@@ -404,4 +395,92 @@ where
         let owned = std::mem::replace(val, zeroed.assume_init());
         std::mem::forget(std::mem::replace(val, f(owned)));
     }
+}
+
+// From an `impl Iterator<Item = type>` return the `type`.
+fn get_item_type(return_type: &syn::ReturnType) -> syn::Type {
+    use syn::{ ReturnType
+             , Type
+             , TypeImplTrait
+             , TypeParamBound
+             , TraitBound
+             , Path
+             , PathSegment
+             , PathArguments
+             , AngleBracketedGenericArguments
+             , GenericArgument
+             , Binding
+             };
+
+    match return_type {
+        ReturnType::Type(_, box Type::ImplTrait(TypeImplTrait { ref bounds, .. })) => {
+            if bounds.len() != 1 {
+                panic!("Expected an impl Iterator as return type");
+            }
+
+            // Assert the function has `impl Iterator` as return type.
+            if let TypeParamBound::Trait(
+                TraitBound {
+                    path: Path {
+                        segments,
+                        ..
+                    },
+                    ..
+                }
+            ) = bounds.first().unwrap() {
+                let PathSegment { ident, .. } = segments.last().unwrap();
+                if ident != "Iterator" {
+                    panic!("Expected an impl Iterator as return type.");
+                }
+
+                if let PathSegment {
+                    arguments: PathArguments::AngleBracketed(
+                        AngleBracketedGenericArguments {
+                            args,
+                            ..
+                        }
+                    ),
+                    ..
+                } = segments.last().unwrap() {
+                    if let GenericArgument::Binding(Binding { ty, .. }) = args.first().unwrap() {
+                        ty.clone()
+                    } else {
+                        panic!("Expected binding in Iterator Item type")
+                    }
+                } else {
+                    panic!("Expected angle bracketed generic arguments")
+                }
+            } else {
+                unreachable!();
+            }
+        },
+        _ => panic!("Expected an impl Iterator as return type."),
+    }
+}
+
+// Generates a vector of identifiers like vec!["Return1", "Return2", ...]
+fn generate_enum_variants(n: usize) -> Vec<syn::Ident> {
+    let mut i = 0;
+    std::iter::from_fn(move || {
+        i += 1;
+        if i <= n {
+            Some(format_ident!("Return{}", i))
+        } else {
+            None
+        }
+    }).collect()
+}
+
+// Generates a vector of n generic parameters like vec!["T1", "T2", ...]
+fn generate_generics(n: usize) -> Vec<syn::GenericParam> {
+    let mut i = 0;
+    std::iter::from_fn(move || {
+        i += 1;
+        if i <= n {
+            let generic = format_ident!("T{}", i);
+            Some(parse_quote!{ #generic })
+        } else {
+            None
+        }
+    }).collect()
 }
